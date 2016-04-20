dc69a643b71e423809a453d52c0df5b5478a401a
Prashanth Ambure <pambure@deloitte.com>
2016-04-20 13:05:57 +0530

    Bug fix for videos not playing on scroll in IE.

--- js/playOnScroll.js
+++ js/playOnScroll.js
@@ -1,6 +1,7 @@
 function inViewPort (elem) {
   //First get the scroll y position (how far the user scrolled down)
-  var scrollY = document.body.scrollTop;
+  //var scrollY = document.body.scrollTop;
+  var scrollY = (document.documentElement.scrollTop) || (document.body.scrollTop);
   //Now get the height of the viewport
   var screenH=document.body.clientHeight;
   //Also get the y position of the element
